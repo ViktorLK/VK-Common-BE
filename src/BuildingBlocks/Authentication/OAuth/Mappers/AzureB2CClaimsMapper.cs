f789d4ff5ea15963908af1223119852ef6303f8e
Kenkon Ro <48170962+ViktorLK@users.noreply.github.com>
2026-03-03 21:53:47 +0900

    Refactor Authentication

--- src/BuildingBlocks/Authentication/OAuth/Mappers/AzureB2CClaimsMapper.cs
+++ src/BuildingBlocks/Authentication/OAuth/Mappers/AzureB2CClaimsMapper.cs
@@ -1,3 +1,8 @@
+using System.Collections.Generic;
+using System.Security.Claims;
+using VK.Blocks.Authentication.Abstractions.Contracts;
+using VK.Blocks.Authentication.Claims;
+
 namespace VK.Blocks.Authentication.OAuth.Mappers;
 
 /// <summary>
@@ -5,4 +10,21 @@ namespace VK.Blocks.Authentication.OAuth.Mappers;
 /// </summary>
 public class AzureB2CClaimsMapper : OAuthClaimsMapperBase
 {
+    /// <inheritdoc />
+    public override IEnumerable<Claim> MapToClaims(OAuthUserInfo userInfo)
+    {
+        // Yield base claims
+        foreach (var claim in base.MapToClaims(userInfo))
+        {
+            yield return claim;
+        }
+
+        // B2C uses 'tfp' (Trust Framework Policy) or 'acr' (Authentication Context Class Reference)
+        // to specify which user flow/custom policy was executed.
+        if (userInfo.Claims.TryGetValue("tfp", out var tfp) ||
+            userInfo.Claims.TryGetValue("acr", out tfp))
+        {
+            yield return new Claim(VKClaimTypes.TrustFrameworkPolicy, tfp);
+        }
+    }
 }
--- src/BuildingBlocks/Authentication/OAuth/Mappers/GitHubClaimsMapper.cs
+++ src/BuildingBlocks/Authentication/OAuth/Mappers/GitHubClaimsMapper.cs
@@ -1,3 +1,8 @@
+using System.Collections.Generic;
+using System.Security.Claims;
+using VK.Blocks.Authentication.Abstractions.Contracts;
+using VK.Blocks.Authentication.Claims;
+
 namespace VK.Blocks.Authentication.OAuth.Mappers;
 
 /// <summary>
@@ -5,4 +10,24 @@ namespace VK.Blocks.Authentication.OAuth.Mappers;
 /// </summary>
 public class GitHubClaimsMapper : OAuthClaimsMapperBase
 {
+    /// <inheritdoc />
+    public override IEnumerable<Claim> MapToClaims(OAuthUserInfo userInfo)
+    {
+        // Yield base claims
+        foreach (var claim in base.MapToClaims(userInfo))
+        {
+            yield return claim;
+        }
+
+        if (userInfo.Claims.TryGetValue("avatar_url", out var avatarUrl))
+        {
+            yield return new Claim(VKClaimTypes.AvatarUrl, avatarUrl);
+        }
+
+        if (userInfo.Claims.TryGetValue("html_url", out var htmlUrl) ||
+            userInfo.Claims.TryGetValue("url", out htmlUrl))
+        {
+            yield return new Claim(VKClaimTypes.ProfileUrl, htmlUrl);
+        }
+    }
 }
